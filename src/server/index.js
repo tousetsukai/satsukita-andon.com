4ab36bbe400486cc3871d764adaf01be8df05aea
amutake <amutake.s@gmail.com>
2015-08-17 10:04:27 +0900

    Suppress eslint errors

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import React from 'react';
 import Location from 'react-router/lib/Location';
 import { Router } from 'react-router';
 import { createStore } from 'redux';
