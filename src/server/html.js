08d87247c9bae48e61b73d9999b3593fb147bdbb
amutake <amutake.s@gmail.com>
2015-08-17 10:31:28 +0900

    indent

--- src/server/html.js
+++ src/server/html.js
@@ -1,19 +1,19 @@
 export default function html(title, meta, link, root, state) {
   return `
-      <!doctype html>
-      <html>
-        <head>
-          <title>${title}</title>
-          <link rel="shortcut icon" href="/assets/img/favicon.ico">
-          ${meta}
-          ${link}
-        </head>
-        <body>
-          <div id="root">${root}</div>
-          <script src="/assets/js/vendor.bundle.js"></script>
-          <script>window.__andon_initial_state=${state};</script>
-          <script src="/assets/js/bundle.js"></script>
-        </body>
-      </html>
+<!doctype html>
+<html>
+  <head>
+    <title>${title}</title>
+    <link rel="shortcut icon" href="/assets/img/favicon.ico">
+    ${meta}
+    ${link}
+  </head>
+  <body>
+    <div id="root">${root}</div>
+    <script src="/assets/js/vendor.bundle.js"></script>
+    <script>window.__andon_initial_state=${state};</script>
+    <script src="/assets/js/bundle.js"></script>
+  </body>
+</html>
   `;
 }
