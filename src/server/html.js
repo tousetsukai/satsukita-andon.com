97acd93c30a3522bca6b8ac9da4df0e488f12113
amutake <amutake.s@gmail.com>
2015-08-17 10:24:23 +0900

    Rewind helmet

--- src/server/html.js
+++ src/server/html.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import serialize from 'serialize-javascript';
-
-export default function html(store, component) {
+export default function html(title, meta, link, root, state) {
   return `
       <!doctype html>
       <html>
         <head>
+          <title>${title}</title>
           <link rel="shortcut icon" href="/assets/img/favicon.ico">
+          ${meta}
+          ${link}
         </head>
         <body>
-          <div id="root">${React.renderToString(component)}</div>
+          <div id="root">${root}</div>
           <script src="/assets/js/vendor.bundle.js"></script>
-          <script>window.__andon_initial_state=${serialize(store.getState())};</script>
+          <script>window.__andon_initial_state=${state};</script>
           <script src="/assets/js/bundle.js"></script>
         </body>
       </html>
--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import React from 'react';
 import Location from 'react-router/lib/Location';
 import { Router } from 'react-router';
 import { createStore } from 'redux';
+import Helmet from 'react-helmet';
+import serialize from 'serialize-javascript';
 import html from './html.js';
 import makeRoot, { routes } from '../universal';
 import reducer from '../reducers';
@@ -12,6 +15,7 @@ app.use('/assets', express.static('assets'));
 
 app.use((req, res) => {
   const location = new Location(req.path, req.query);
+  console.log(`location: ${req.path}`);
   const store = createStore(reducer);
   Router.run(routes, location, (error, initialState, transition) => {
     if (error) {
@@ -19,7 +23,10 @@ app.use((req, res) => {
     } else if (transition.isCancelled && transition.redirectInfo) {
       res.redirect(transition.redirectInfo.pathname);
     } else {
-      res.send(html(store, makeRoot(store, initialState)));
+      const rootString = React.renderToString(makeRoot(store, initialState));
+      const { title, meta, link } = Helmet.rewind();
+      const serializedState = serialize(store.getState());
+      res.send(html(title, meta, link, rootString, serializedState));
     }
   });
 });
