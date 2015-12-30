d17eb075a3aaab7d28cebeeb8bfc4c0b8bcfb850
amutake <amutake.s@gmail.com>
2015-12-30 12:55:47 +0900

    Introduce redux-simple-router

--- src/client.js
+++ src/client.js
@@ -1,5 +1,6 @@
 import { render } from 'react-dom';
 import { createHistory } from 'history';
+import { syncReduxAndRouter } from 'redux-simple-router';
 
 import { createClientApp, configureStore } from './universal';
 
@@ -7,6 +8,8 @@ const initialState = window.__INITIAL_STATE__;
 
 const store = configureStore(initialState);
 const history = createHistory();
+syncReduxAndRouter(history, store);
+
 const app = createClientApp(store, history);
 
 render(app, document.getElementById('app'));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { gallery } from './gallery';
+import { routeReducer } from 'redux-simple-router';
 
 export default combineReducers({
-  gallery: gallery
+  gallery: gallery,
+  routing: routeReducer
 });
