b7596e2e694a6b9dd115e22860360d265e240681
amutake <amutake.s@gmail.com>
2016-01-02 17:47:56 +0900

    top logo

--- src/containers/home.js
+++ src/containers/home.js
@@ -4,6 +4,24 @@ import { connect } from 'react-redux';
 import _ from 'lodash';
 
 import { getTopNews } from '../actions';
+import useSheet from '../jss';
+
+const sheet = {
+  // ref: http://stackoverflow.com/questions/7273338/how-to-vertically-align-an-image-inside-div
+  logoWrapper: {
+    width: '100%',
+    height: 400,
+    'text-align': 'center',
+  },
+  logoHelper: {
+    display: 'inline-block',
+    height: '100%',
+    'vertical-align': 'middle',
+  },
+  logo: {
+    'vertical-align': 'middle',
+  },
+};
 
 class Home extends Component {
 
@@ -18,13 +36,18 @@ class Home extends Component {
   }
 
   render() {
-    const { topNews } = this.props;
+    const { sheet, topNews } = this.props;
+    const { classes } = sheet;
     return (
       <div>
         <Helmet
           title="行灯職人への道"
           titleTemplate="%s"
         />
+        <div className={classes.logoWrapper}>
+          <span className={classes.logoHelper}/>
+          <img className={classes.logo} src="/static/img/logo.png"/>
+        </div>
         <h3>News</h3>
         <p dangerouslySetInnerHTML={{__html: topNews.body}}></p>
       </div>
@@ -32,8 +55,8 @@ class Home extends Component {
   }
 }
 
-export default connect(
+export default useSheet(connect(
   state => ({
     topNews: state.home.topNews,
   })
-)(Home);
+)(Home), sheet);
