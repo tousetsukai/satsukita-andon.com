2f40c3bef05550d3ea227fd1df6100a1c721d79f
amutake <amutake.s@gmail.com>
2016-01-02 12:11:54 +0900

    real path

--- src/components/festival-thumbnail.js
+++ src/components/festival-thumbnail.js
@@ -11,7 +11,7 @@ class FestivalThumbnail extends React.Component {
 
   static propTypes = {
     festival: T.shape({
-      thumbnail_url: T.string, // .isRequired,
+      thumbnail_url: T.string,
       times_ord: T.string.isRequired,
       theme: T.string.isRequired,
       theme_roman: T.string.isRequired,
@@ -26,10 +26,11 @@ class FestivalThumbnail extends React.Component {
   render() {
     const { sheet, festival } = this.props;
     const { classes } = sheet;
+    const thumbnail = festival.thumbnail_url || '/static/img/no-icon.svg';
     return (
       <img className={classes.thumbnail}
            width={1024 / 4}
-           src={`https://satsukita-andon.com/files/grands/${festival.times_ord}.jpg`}/>
+           src={thumbnail}/>
     );
   }
 }
