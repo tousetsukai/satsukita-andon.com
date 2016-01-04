3c721730b24a6bba33c6acb8549039f73da01c7d
amutake <amutake.s@gmail.com>
2016-01-05 01:40:38 +0900

    postcss: header

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ module.exports = {
   output: 'static/bundle.css',
   use: [
     'postcss-import', // 'postcss-import' should be first in the plugin list
+    'postcss-mixins', // 'postcss-mixins' should be before 'postcss-nested' and 'postcss-simple-vars'
     'autoprefixer',
     'postcss-simple-vars',
     'postcss-calc',
--- src/components/header.js
+++ src/components/header.js
@@ -2,76 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import _ from 'lodash';
 
-import useSheet from '../jss';
-import color from '../jss/color';
-import size from '../jss/size';
-import { ghostButton } from '../jss/util';
 import UserDropdown from './user-dropdown';
-
-const logoMargin = 3;
-
-const sheet = {
-  header: {
-    'background-color': 'rgba(0, 0, 0, 0.6)',
-    transition: '0.1s linear',
-    height: size.headerHeight,
-    width: '100%',
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    'box-shadow': '0 0 3px 3px rgba(0, 0, 0, 1)',
-    'z-index': '10000',
-    '&:hover': {
-      'background-color': 'rgba(0, 0, 0, 0.9)',
-    },
-  },
-  wrapper: {
-    margin: 'auto',
-    padding: '0 8px',
-    display: 'flex',
-    'justify-content': 'space-between',
-  },
-  navUl: {
-    display: 'flex',
-    'align-items': 'center',
-    height: size.headerHeight,
-    '& h1': { // to reset elemental.css
-      'font-size': 0,
-      margin: 0,
-    },
-  },
-  logo: {
-    height: size.headerHeight - logoMargin * 2,
-  },
-  menu: {
-    'font-size': size.headerHeight / 3,
-    color: color.text,
-    'text-decoration': 'none',
-    'margin-left': 30,
-    '&:hover, &:focus': {
-      'text-decoration': 'none',
-      color: color.theme,
-    },
-  },
-  right: {
-    display: 'flex',
-    'align-items': 'center',
-    height: size.headerHeight,
-  },
-  button: {
-    ...ghostButton(color.green, color.text),
-    'font-size': size.headerHeight / 3.4,
-    'margin-left': 14,
-  },
-  search: {
-    'margin-right': 30,
-  },
-  '@media (min-width: 1024px)': {
-    wrapper: {
-      width: '1024px',
-    },
-  },
-};
 
 class Header extends Component {
 
@@ -80,14 +11,12 @@ class Header extends Component {
   }
 
   signupOrUser() {
-    const { sheet } = this.props;
-    const { classes } = sheet;
     const user = this.context.user;
 
     if (_.isEmpty(user)) {
       return [
-        <Link key={0} className={classes.button} to="/signup">新規登録</Link>,
-        <Link key={1} className={classes.button} to="/signin">ログイン</Link>,
+        <Link key={0} className="menu-button" to="/signup">新規登録</Link>,
+        <Link key={1} className="menu-button" to="/signin">ログイン</Link>,
       ];
     } else {
       return <UserDropdown/>;
@@ -95,26 +24,23 @@ class Header extends Component {
   }
 
   render() {
-    const { sheet } = this.props;
-    const { classes } = sheet;
-
     return (
-      <header className={classes.header}>
-        <div className={classes.wrapper}>
+      <header className="header">
+        <div className="header-wrapper">
           <nav>
-            <ul className={classes.navUl}>
+            <ul className="header-left">
               <li>
                 <h1>
                   <Link to="/">
-                    <img className={classes.logo} src="/static/img/logo.png"/>
+                    <img className="logo" src="/static/img/logo.png"/>
                   </Link>
                 </h1>
               </li>
-              <li><Link className={classes.menu} to="/gallery">Gallery</Link></li>
-              <li><Link className={classes.menu} to="/howto">How to</Link></li>
+              <li><Link className="menu-link" to="/gallery">Gallery</Link></li>
+              <li><Link className="menu-link" to="/howto">How to</Link></li>
             </ul>
           </nav>
-          <div className={classes.right}>
+          <div className="header-right">
             {this.signupOrUser()}
           </div>
         </div>
@@ -123,4 +49,4 @@ class Header extends Component {
   }
 }
 
-export default useSheet(Header, sheet);
+export default Header;
