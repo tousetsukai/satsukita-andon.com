fdde28a310ed306e137afa70c766d06410ca1f72
amutake <amutake.s@gmail.com>
2016-01-05 12:54:17 +0900

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,11 +7,11 @@ class Footer extends Component {
     return (
       <footer className="footer">
         <div className="footer-wrapper">
-          <ul className="footer-left">
-            <li><Link className="footer-link" to="/about">このサイトについて</Link></li>
-            <li>|</li>
-            <li><Link className="footer-link" to="/contact">お問い合わせ・質問</Link></li>
-          </ul>
+          <p className="footer-left">
+            <Link className="footer-link" to="/about">このサイトについて</Link>
+            <span className="space">    |    </span>
+            <Link className="footer-link" to="/contact">お問い合わせ・質問</Link>
+          </p>
           <p>copyright &copy; 2002-2016 行灯職人への道</p>
         </div>
       </footer>
