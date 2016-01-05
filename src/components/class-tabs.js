dde5806f5cae7158a2c0cb3091c06954038a628b
amutake <amutake.s@gmail.com>
2016-01-05 14:25:03 +0900

    postcss: class-tabs

--- src/components/class-tabs.js
+++ src/components/class-tabs.js
@@ -2,42 +2,6 @@ import React from 'react';
 import { Link } from 'react-router';
 import classnames from 'classnames';
 
-import useSheet from '../jss';
-import color from '../jss/color';
-
-const sheet = {
-  tabs: {
-    display: 'flex',
-    'justify-content': 'space-around',
-    width: '100%',
-    margin: '30px 0 30px',
-  },
-  tab: {
-    'list-style-type': 'none',
-    display: 'block',
-    width: 120,
-  },
-  text: {
-    display: 'inline-block',
-    color: color.text,
-    width: '100%',
-    'text-align': 'center',
-    'font-size': 16,
-    '&:hover, &:focus': {
-      color: color.theme,
-      'text-decoration': 'none',
-    },
-  },
-  activeTab: {
-    'border-bottom': '3px solid',
-    'border-color': color.theme,
-    'padding-bottom': 3,
-  },
-  activeText: {
-    color: color.theme,
-  },
-};
-
 class ClassTabs extends React.Component {
 
   static propTypes = {
@@ -45,10 +9,9 @@ class ClassTabs extends React.Component {
   }
 
   render() {
-    const { sheet, clazz } = this.props;
+    const { clazz } = this.props;
     const path = `/gallery/${clazz.times_ord}/${clazz.grade}-${clazz['class']}`;
     const tabName = this.props.tab;
-    const { classes } = sheet;
     const tabs = [
       { tab: 'basic', label: '基本情報' },
       { tab: 'reviews', label: '講評' },
@@ -59,20 +22,20 @@ class ClassTabs extends React.Component {
       const isActive = tabName === tab;
       return (
         <li key={tab}
-            className={classnames({ [classes.tab]: true, [classes.activeTab]: isActive })}>
+            className={classnames({ 'class-tab': true, 'class-tab-active': isActive })}>
           <Link to={{ pathname: path + '/' + tab }}
-                className={classnames({ [classes.text]: true, [classes.activeText]: isActive })}>
+                className={classnames({ 'class-text': true, 'class-text-active': isActive })}>
             {label}
           </Link>
         </li>
       );
     });
     return (
-      <ul className={classes.tabs}>
+      <ul className="class-tabs">
         {tabs}
       </ul>
     );
   }
 }
 
-export default useSheet(ClassTabs, sheet);
+export default ClassTabs;
