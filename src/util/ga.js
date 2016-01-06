19a2ec09bd08c067370eee18952b87c1a560d3c7
amutake <amutake.s@gmail.com>
2016-01-06 21:07:54 +0900

    no console

--- src/util/ga.js
+++ src/util/ga.js
@@ -1,6 +1,5 @@
 export default function (path) {
   if (typeof window !== 'undefined' && typeof window.ga !== 'undefined') {
-    console.log('ga!');
     window.ga('send', 'pageview', path);
   }
 }
