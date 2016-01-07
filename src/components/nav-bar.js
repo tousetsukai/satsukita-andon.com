f0656b08ceb15c869dbe547a09fba2fc6bbab6e2
amutake <amutake.s@gmail.com>
2016-01-07 21:36:10 +0900

    Fix nav-bar styles

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -15,14 +15,16 @@ export default class NavBar extends React.Component {
     const { items, activeId } = this.props;
     return (
       <div className="nav-bar">
-        <ul className="nav-bar-items">
-          {items.map(({ link, label, id }) => (
-            <li className={classnames({ 'nav-bar-item': true, 'nav-bar-item-active': activeId === id })}
-                key={id}>
-              <Link className="nav-bar-item-link" to={link}>{label}</Link>
-            </li>
-          ))}
-        </ul>
+        <div className="nav-bar-shadow">
+          <ul className="nav-bar-items">
+            {items.map(({ link, label, id }) => (
+              <li className={classnames({ 'nav-bar-item': true, 'nav-bar-item-active': activeId === id })}
+                  key={id}>
+                <Link className="nav-bar-item-link" to={link}>{label}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
