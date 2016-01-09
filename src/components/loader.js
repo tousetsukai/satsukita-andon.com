239ec7263112caf57606ba387bbfd27a2b2744c3
amutake <amutake.s@gmail.com>
2016-01-09 16:24:24 +0900

    Fix loader position

--- src/components/loader.js
+++ src/components/loader.js
@@ -4,9 +4,7 @@ export default class Loader extends React.Component {
   render() {
     return (
       <div className="loader-wrapper">
-        <div>
-          <div className="loader">Loading...</div>
-        </div>
+        <div className="loader"/>
       </div>
     );
   }
