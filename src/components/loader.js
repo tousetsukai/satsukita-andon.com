b03a2b44934d7fb44e2bb0fb6dd962d900b752fe
amutake <amutake.s@gmail.com>
2016-01-09 14:34:10 +0900

    Improve loader

--- src/components/loader.js
+++ src/components/loader.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default class Loader extends React.Component {
   render() {
-    return (<div className="loader">Loading...</div>);
+    return (
+      <div className="loader-wrapper">
+        <div>
+          <div className="loader">Loading...</div>
+        </div>
+      </div>
+    );
   }
 }
