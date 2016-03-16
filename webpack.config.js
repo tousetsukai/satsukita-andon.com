5b8c39aadad1de57e9b168a026d2d4f2749a68a6
amutake <amutake.s@gmail.com>
2016-03-16 20:31:58 +0900

    Always NODE_ENV=production

--- webpack.config.js
+++ webpack.config.js
@@ -33,10 +33,10 @@ module.exports = {
       compress: { warnings: false },
     }),
     new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
-    /* new webpack.DefinePlugin({
-       "process.env": {
-       NODE_ENV: JSON.stringify("production"),
-       },
-       }), */
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
   ]
 };
