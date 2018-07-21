604c032a5c1c54274656734611cff481ec03a603
amutake <amutake.s@gmail.com>
2018-07-21 17:00:11 +0900

    Add bulma

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const handler = routes.getRequestHandler(app)
 // with express
 const express = require('express')
 app.prepare().then(() => {
-  express().use(handler).listen(5927) // 5927 represents 59th2-7 :)
+  express().use(handler).listen(3000)
 })
