7981be4414ac7d7840f371972dcec261bc620429
whatasoda <a16dc107@dhw.ac.jp>
2018-10-25 00:41:43 +0900

    Add: add fork-ts-checker-webpack-plugin

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,6 +1,22 @@
 import withTypescript = require('@zeit/next-typescript');
+import ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+import { ServerOptions } from 'next';
+import { Configuration } from 'webpack';
 
 export = withTypescript({
   // relative path from next.config.js
   distDir: '../../dist/functions/next',
+  webpack(config: Configuration, options: ServerOptions) {
+    config.plugins = config.plugins || [];
+
+    if (options.dev && options.isServer) {
+      config.plugins.push(
+        new ForkTsCheckerWebpackPlugin({
+          tsconfig: '../../tsconfig.json',
+        }),
+      );
+    }
+
+    return config;
+  },
 });
