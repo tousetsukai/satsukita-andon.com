8e4744f143d51031c14b873a005594b0bb88a47c
amutake <amutake.s@gmail.com>
2020-11-04 10:00:37 +0900

    Fix functions

--- functions/index.ts
+++ functions/index.ts
@@ -1,12 +1,15 @@
 import * as functions from 'firebase-functions';
 import next from 'next';
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev, conf: { distDir: 'next' } });
+// dev: false しかサポートしない。.next を dist/functions 以下に置くようにする
+// - dev: true の場合、pages ディレクトリがあるディレクトリを dir で指定する必要があり、
+//   `../../` などで指定することになるが、functions の実行ディレクトリは /srv なので、デプロイに失敗する
+// - dev: true の場合、firebase serve で無限リロードが発生してしまう
+const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
 export const nextApp = functions.https.onRequest((req, res) => {
   // eslint-disable-next-line no-console
-  console.log(`File: ${req.originalUrl}`);
+  console.log(`Requested URL: ${req.originalUrl}`);
   return app.prepare().then(() => handle(req, res));
 });
