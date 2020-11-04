0fd9be8e15fca66f0c3023a9585218ecdf764bac
amutake <amutake.s@gmail.com>
2020-11-04 11:45:59 +0900

    Fix firebase deploy

--- functions/index.ts
+++ functions/index.ts
@@ -1,14 +1,16 @@
+import path from 'path';
 import * as functions from 'firebase-functions';
 import next from 'next';
 
-// dev: false しかサポートしない。.next を dist/functions 以下に置くようにする
+// { dev: false } しかサポートしない。.next を dist/functions 以下に置くようにする
+// ref: // https://github.com/vercel/next.js/issues/8893#issuecomment-538991508
 // - dev: true の場合、pages ディレクトリがあるディレクトリを dir で指定する必要があり、
 //   `../../` などで指定することになるが、functions の実行ディレクトリは /srv なので、デプロイに失敗する
 // - dev: true の場合、firebase serve で無限リロードが発生してしまう
 const app = next({
   dev: false,
   conf: {
-    distDir: '.next',
+    distDir: `${path.relative(process.cwd(), __dirname)}/.next`,
   },
 });
 const handle = app.getRequestHandler();
